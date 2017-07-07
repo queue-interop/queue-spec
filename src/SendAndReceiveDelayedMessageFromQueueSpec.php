7ad07e0e8e80cd9ab776ab891b566a35f14b9d36
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-07-07 16:46:25 +0300

    add spec for delayed messages

---
+++ src/SendAndReceiveDelayedMessageFromQueueSpec.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Interop\Queue\Spec;
+
+use Interop\Queue\PsrContext;
+use Interop\Queue\PsrMessage;
+use Interop\Queue\PsrQueue;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group functional
+ */
+abstract class SendAndReceiveDelayedMessageFromQueueSpec extends TestCase
+{
+    public function test()
+    {
+        $context = $this->createContext();
+        $queue = $this->createQueue($context, 'send_and_receive_delayed_message_from_queue_spec');
+
+        $consumer = $context->createConsumer($queue);
+
+        // guard
+        $this->assertNull($consumer->receiveNoWait());
+
+        $expectedBody = __CLASS__.time();
+
+        $producer = $context->createProducer();
+        $producer->setDeliveryDelay(5000); // 5sec
+        $producer->send($queue, $context->createMessage($expectedBody));
+
+        $sendAt = microtime(true);
+
+        $message = $consumer->receive(8000); // 8 sec
+
+        $this->assertInstanceOf(PsrMessage::class, $message);
+        $consumer->acknowledge($message);
+        $this->assertSame($expectedBody, $message->getBody());
+
+        $this->assertGreaterThanOrEqual(5, microtime(true) - $sendAt);
+    }
+
+    /**
+     * @return PsrContext
+     */
+    abstract protected function createContext();
+
+    /**
+     * @param PsrContext $context
+     * @param string     $queueName
+     *
+     * @return PsrQueue
+     */
+    protected function createQueue(PsrContext $context, $queueName)
+    {
+        return $context->createQueue($queueName);
+    }
+}
