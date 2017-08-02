1a03ced15e626c519048179023e7feab4554d22e
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-02 17:42:48 +0300

    add spec for ttl and priority

---
+++ src/SendAndReceivePriorityMessagesFromQueueSpec.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Interop\Queue\Spec;
+
+use Interop\Queue\PsrContext;
+use Interop\Queue\PsrMessage;
+use Interop\Queue\PsrQueue;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group functional
+ */
+abstract class SendAndReceivePriorityMessagesFromQueueSpec extends TestCase
+{
+    public function test()
+    {
+        $context = $this->createContext();
+        $queue = $this->createQueue($context, 'send_and_receive_priority_messages_from_queue_spec');
+
+        $consumer = $context->createConsumer($queue);
+
+        // guard
+        $this->assertNull($consumer->receiveNoWait());
+
+        $expectedPriority5Body = __CLASS__.'_priority5_'.time();
+        $expectedPriority1Body = __CLASS__.'_priority1_'.time();
+
+        $producer = $context->createProducer();
+
+        $producer->setPriority(1);
+        $producer->send($queue, $context->createMessage($expectedPriority1Body));
+
+        $producer->setPriority(5);
+        $producer->send($queue, $context->createMessage($expectedPriority5Body));
+
+        $message = $consumer->receive(8000); // 8 sec
+
+        $this->assertInstanceOf(PsrMessage::class, $message);
+        $consumer->acknowledge($message);
+        $this->assertSame($expectedPriority5Body, $message->getBody());
+
+        $message = $consumer->receive(8000); // 8 sec
+
+        $this->assertInstanceOf(PsrMessage::class, $message);
+        $consumer->acknowledge($message);
+        $this->assertSame($expectedPriority1Body, $message->getBody());
+    }
+
+    /**
+     * @return PsrContext
+     */
+    abstract protected function createContext();
+
+    /**
+     * @param PsrContext $context
+     * @param string     $queueName
+     *
+     * @return PsrQueue
+     */
+    protected function createQueue(PsrContext $context, $queueName)
+    {
+        return $context->createQueue($queueName);
+    }
+}
---
+++ src/SendAndReceiveTimeToLiveMessagesFromQueueSpec.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Interop\Queue\Spec;
+
+use Interop\Queue\PsrContext;
+use Interop\Queue\PsrMessage;
+use Interop\Queue\PsrQueue;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group functional
+ */
+abstract class SendAndReceiveTimeToLiveMessagesFromQueueSpec extends TestCase
+{
+    public function test()
+    {
+        $context = $this->createContext();
+        $queue = $this->createQueue($context, 'send_and_receive_time_to_live_messages_from_queue_spec');
+
+        $consumer = $context->createConsumer($queue);
+
+        // guard
+        $this->assertNull($consumer->receiveNoWait());
+
+        $expectedBody = __CLASS__.time();
+
+        $producer = $context->createProducer();
+
+        $producer->setTimeToLive(2000);
+        $producer->send($queue, $context->createMessage('it should not be received'));
+
+        $producer->setTimeToLive(null);
+        $producer->send($queue, $context->createMessage($expectedBody));
+
+        sleep(3);
+
+        $message = $consumer->receive(4000); // 8 sec
+
+        $this->assertInstanceOf(PsrMessage::class, $message);
+        $consumer->acknowledge($message);
+        $this->assertSame($expectedBody, $message->getBody());
+    }
+
+    /**
+     * @return PsrContext
+     */
+    abstract protected function createContext();
+
+    /**
+     * @param PsrContext $context
+     * @param string     $queueName
+     *
+     * @return PsrQueue
+     */
+    protected function createQueue(PsrContext $context, $queueName)
+    {
+        return $context->createQueue($queueName);
+    }
+}
