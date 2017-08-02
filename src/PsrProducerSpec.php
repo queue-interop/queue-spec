15da02d715ad5d3effd7dbaa52ba9ad24e116198
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-02 16:56:24 +0300

    remove not relevant tests.

--- src/PsrProducerSpec.php
+++ src/PsrProducerSpec.php
@@ -14,53 +14,6 @@ abstract class PsrProducerSpec extends TestCase
         $this->assertInstanceOf(PsrProducer::class, $this->createProducer());
     }
 
-    public function testShouldReturnNullIfCompletionListenerWasNotPreviouslySet()
-    {
-        $producer = $this->createProducer();
-
-        $this->assertNull($producer->getCompletionListener());
-    }
-
-    public function testShouldReturnPreviouslySetCompletionListener()
-    {
-        $completionListener = $this->createMock(CompletionListener::class);
-
-        $producer = $this->createProducer();
-        $producer->setCompletionListener($completionListener);
-
-        $this->assertSame($completionListener, $producer->getCompletionListener());
-    }
-
-    public function testShouldAllowResetPreviouslySetCompletionListener()
-    {
-        $producer = $this->createProducer();
-
-        $producer->setCompletionListener($this->createMock(CompletionListener::class));
-
-        // guard
-        $this->assertNotNull($producer->getCompletionListener());
-
-        $producer->setCompletionListener(null);
-
-        $this->assertNull($producer->getCompletionListener());
-    }
-
-    public function testShouldReturnDefaultDeliveryDelayIfNotPreviouslySet()
-    {
-        $producer = $this->createProducer();
-
-        $this->assertSame(PsrMessage::DEFAULT_DELIVERY_DELAY, $producer->getDeliveryDelay());
-    }
-
-    public function testShouldReturnPreviouslySetDeliveryDelay()
-    {
-        $producer = $this->createProducer();
-
-        $producer->setDeliveryDelay(123.45);
-
-        $this->assertSame(123.45, $producer->getDeliveryDelay());
-    }
-
     /**
      * @return PsrProducer
      */
